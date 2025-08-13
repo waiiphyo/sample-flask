cb82d3fe5b77c91d71095166a42f5adeeac47c09
waiphyo <99342958+waiiphyo@users.noreply.github.com>
2025-08-13 20:30:42 +0630

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Flask on Docker!'
+    return 'Hello, Flask on Docker! clickup webhook'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
