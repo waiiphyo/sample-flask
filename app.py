800c0b14eb06553ae6eda395e58b3dbc2ed9afa7
waiphyo <99342958+waiiphyo@users.noreply.github.com>
2025-08-18 10:21:40 +0630

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Flask on Docker! add clickup channel webhook'
+    return 'Hello, Flask on Docker! add clickup channel webhook | scan with trivy image scanner'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
